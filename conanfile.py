05f7cb4b061ad23570a64abad985dfaa29e3a65b
Victor GALLET <vic03@live.fr>
2018-02-28 11:38:29 +0100

    Add the project

---
+++ conanfile.py
@@ -0,0 +1,38 @@
+from conans import ConanFile, CMake
+
+
+class EtherConan(ConanFile):
+    name = "ether"
+    version = "0.0.1"
+    license = "<Put the package license here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Ether here>"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+    exports_sources = "ether/*"
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin") # From bin to bin
+        self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="ether")
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake %s/src %s' % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="include", src="ether/include")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["hello"]
